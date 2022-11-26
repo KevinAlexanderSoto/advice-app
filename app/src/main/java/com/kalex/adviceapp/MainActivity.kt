fd1166fbec329b3b02d4ef0e540e9687cda2548d
Kevin Soto <kevinalexandersoto999@gmail.com>
2022-11-26 13:58:28 -0500

    refactor : delete unused code and refactor

--- app/src/main/java/com/kalex/adviceapp/MainActivity.kt
+++ app/src/main/java/com/kalex/adviceapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.kalex.adviceapp
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -15,7 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,7 +27,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.kalex.adviceapp.ui.theme.AdviceAppTheme
-import retrofit2.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
@@ -33,15 +38,16 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val client = Retrofit.Builder()
             .baseUrl("https://api.adviceslip.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(UserRetroApi ::class.java)
+            .create(UserRetroApi::class.java)
 
         setContent {
             AdviceAppTheme {
-                var result  by mutableStateOf("")
+                var result by mutableStateOf("")
 
                 Surface(
                     modifier = Modifier
@@ -56,7 +62,7 @@ class MainActivity : ComponentActivity() {
                             .verticalScroll(rememberScrollState()),// para hacer scroll
                         verticalArrangement = Arrangement.spacedBy(11.dp),
                         horizontalAlignment = Alignment.CenterHorizontally
-                    ){
+                    ) {
 
                         Text(
                             text = "Advice APP",
@@ -66,7 +72,7 @@ class MainActivity : ComponentActivity() {
                         )
 
                         Advice(result)
-                        GetAdviceButton(client){ it ->
+                        GetAdviceButton(client) {
                             result = it
                         }
                     }
@@ -79,20 +85,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Advice(name: String) {
-    Text(modifier = Modifier.padding(15.dp),text = "${name}")
+    Text(modifier = Modifier.padding(15.dp), text = name)
 }
 
 @Composable
-fun GetAdviceButton(client: UserRetroApi, function: (String) -> Unit){
+fun GetAdviceButton(client: UserRetroApi, function: (String) -> Unit) {
     val context = LocalContext.current
-
     Button(
         onClick = {
-
-            client.getUser().enqueue(object : Callback<splip>{
+            client.getUser().enqueue(object : Callback<splip> {
                 override fun onResponse(call: Call<splip>, response: Response<splip>) {
-                    val Advice = response.body()?.slip
-                    function(Advice?.advice.toString())
+                    val adviceResponse = response.body()?.slip
+                    function(adviceResponse?.advice.toString())
 
                 }
 
@@ -108,13 +112,12 @@ fun GetAdviceButton(client: UserRetroApi, function: (String) -> Unit){
         },
         modifier = Modifier
             .padding(top = 30.dp)
-            .fillMaxWidth(0.8f)
-
-        ,
+            .fillMaxWidth(0.8f),
         border = BorderStroke(1.dp, Color.Black),
         shape = RoundedCornerShape(23.dp),
-        contentPadding = PaddingValues(12.dp)
-    ) {
+        contentPadding = PaddingValues(12.dp),
+
+        ) {
         Text(
             text = "Get Advice ",
             fontSize = 20.sp,
@@ -123,41 +126,19 @@ fun GetAdviceButton(client: UserRetroApi, function: (String) -> Unit){
     }
 }
 
-fun callApi(context: Context, client: UserRetroApi) {
-
-    return client.getUser().enqueue(object : Callback<splip>{
-        override fun onResponse(call: Call<splip>, response: Response<splip>) {
-            val Advice = response.body()?.slip
-            Toast.makeText(
-                context,
-                Advice?.advice.toString(),
-                Toast.LENGTH_LONG
-            ).show()
-
-        }
-
-        override fun onFailure(call: Call<splip>, t: Throwable) {
-            Toast.makeText(
-                context,
-                t.message ?: "ERROR",
-                Toast.LENGTH_LONG
-            ).show()
-        }
-
-    })
-}
-
 interface UserRetroApi {
 
     @GET("advice")
-     fun getUser(
+    fun getUser(
     ): Call<splip>
 
 }
+
 data class splip(
-    val slip: Advicedto = Advicedto("1","ADVICE 1")
+    val slip: Advicedto = Advicedto("1", "ADVICE 1")
 )
+
 data class Advicedto(
-    val id: String ="",
-    val advice: String ="",
+    val id: String = "",
+    val advice: String = "",
 )
